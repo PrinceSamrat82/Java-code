01074fad973c9ec5e012a61a51791dffff4ccd11
Prince <princekr081001@gmail.com>
2024-03-11 10:55:44 +0530

    first commit

---
+++ testArray.java
@@ -0,0 +1,16 @@
+public class testArray {
+    public static void main(String[] args) {
+        int a[]={10,20,30,40,50};
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        
+        float sum=0 ,avg;
+        sum +=a[i];
+        avg=sum/a.length;
+        System.out.println("Avergae =" +avg);
+      }
+
+        
+ }
+    
+}
