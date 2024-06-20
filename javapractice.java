e2b7f35b1a9a5f28abc02c182bcb6f9d91ada686
Prince <princekr081001@gmail.com>
2024-06-20 21:53:15 +0530

    first commit

---
+++ javapractice.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class javapractice {
+    public static void main(String[] args) {
+        
+        String family = " all member ";
+        int age= 23;
+        float age1 = 23.8f;
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(family);
+        System.out.println(a);
+         
+
+    }
+    
+}
---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[]args){
+        String name=new String("Prince");
+        int []marks=new int[3];
+         marks[0]= 98;
+         marks[1]=99;
+         marks[2]=100;
+         final float PI=3.14f;
+
+        System.out.println("Hello World");
+        System.out.println(marks[1] );
+        System.out.println(PI);
+        System.out.println(name);
+
+    }
+    
+}
