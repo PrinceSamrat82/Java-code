694a6779136bf8232ec56e6dfadfbd34aa72f904
Prince <princekr081001@gmail.com>
2024-09-29 22:22:24 +0530

    first commit

---
+++ recursion6.java
@@ -0,0 +1,34 @@
+public class recursion6 {
+
+    // Remove duplicates in a String " abbccda"?
+    //Time complexity is O(n) Where n is length of string. 
+
+    public static boolean[]map =new boolean[26];
+    public static void removeDuplicates( String str, int index, String newString){
+
+        if (index==str.length()) {
+
+            System.out.println(newString);
+            return;
+            
+        }
+        char currChar =str.charAt(index);
+
+        if (map[currChar-'a']) {
+            removeDuplicates(str, index+1, newString);
+            
+        }else{
+            newString+=currChar;
+            map[currChar-'a']=true;
+            removeDuplicates(str, index+1, newString);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        String str = "abbccda";
+        removeDuplicates(str, 0, "");
+    }
+    
+}
